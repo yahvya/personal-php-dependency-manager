149e158911ee76c6efe31eca5a7d50d57ea32f0a
Yahaya Bathily <yahayad.dev@gmail.com>
2025-08-14 16:01:30 +0200

    ADD the main manager methods prototypes (getInstance,registerSingleton,register,call)

--- Src/DependencyManager.php
+++ Src/DependencyManager.php
@@ -2,8 +2,11 @@
 
 namespace Yahay\PhpDependencyInjector;
 
+use Closure;
+
 /**
- * Dependencies manager. Singleton
+ * Dependencies manager
+ * @notice Pseudo singleton class. You can use it like that or you can use DependencyManager::get() which will return the same instance. The constructor is opened
  */
 class DependencyManager
 {
@@ -13,11 +16,9 @@ class DependencyManager
     protected static ?DependencyManager $instance = null;
 
     /**
-     * Singleton protection
+     * @var array{string:} Dependencies map
      */
-    protected function __construct()
-    {
-    }
+    protected(set) array $dependenciesMap = [];
 
     /**
      * Provide a registered dependency
@@ -28,6 +29,44 @@ class DependencyManager
     public function getDependency(string $class):object
     {
         throw new DependencyManagerException(errorType: DependencyManagerError::DEPENDENCY_NOT_FOUND);
+    }
+
+    /**
+     * @template T Callable return type
+     * Call a callable by injecting the dependencies
+     * @param callable():T $toCall Callable
+     * @param array $manualParameters Parameters to inject manually
+     * @return T Callable return type
+     * @throws DependencyManagerException On error
+     */
+    public function call(Callable $toCall,array $manualParameters = []):mixed
+    {
+        throw new DependencyManagerException(errorType: DependencyManagerError::DEPENDENCY_NOT_FOUND);
+    }
+
+    /**
+     * Register a singleton dependency builder
+     * @param string $class Class name
+     * @param Closure $builder Dependency builder (The non-specified builder parameters will be injected automatically if required)
+     * @param array $manualParameters Parameters to inject manually
+     * @return $this
+     */
+    public function registerSingleton(string $class,Closure $builder,array $manualParameters = []): DependencyManager
+    {
+        return $this;
+    }
+
+    /**
+     * Register a dependency builder
+     * @param string $class Class name
+     * @param Closure $builder Dependency builder (The non-specified builder parameters will be injected automatically if required)
+     * @param array $manualParameters Parameters to inject manually
+     * @attention The build closure will be called each time this dependency is requested
+     * @return $this
+     */
+    public function register(string $class,Closure $builder,array $manualParameters = []): DependencyManager
+    {
+        return $this;
     }
 
     /**
